d62f09cec4ad836b30ec1c60a2b4bb73243a9c02
swolepenguin <ashtonm223@gmail.com>
2020-12-01 09:55:34 -0500

    user db creation and migration

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios'); 
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.set('view engine','ejs')
