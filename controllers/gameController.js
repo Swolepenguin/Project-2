ac200b9dc47639d15a1a2e59a1a3ff4fb2b247f4
swolepenguin <ashtonm223@gmail.com>
2020-12-02 10:28:51 -0500

    favorites route

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,8 +2,20 @@ const db = require('../models')
 const axios = require('axios')
 let router = express.router()
 
+router.post('/faves',(req,res)=>{
+    db.favorites.create({
+        title: req.body.title
+    })
+})
+
 router.get('/faves',(req,res)=>{
-    
+    db.favorites.findAll().then(allFaves =>{
+        res.render('/views/faves', {allFaves})
+    })
+})
+
+router.get('/login',(req,res)=>{
+
 })
 
 module.exports = router
