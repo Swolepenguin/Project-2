22da2764999f5ecf8268c2bfec041f0f974d0de3
swolepenguin <ashtonm223@gmail.com>
2020-12-02 11:39:44 -0500

    landing page

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+
 const port = process.env.PORT || 4000;
 
 app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.render('views/landing')
+    res.render('landing')
 })
 
 app.listen(4000)
